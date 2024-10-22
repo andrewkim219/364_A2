4670c722d094305929752f1068c522c4a2bc3726
Andrew Kim <126522014+andrewkim219@users.noreply.github.com>
2024-10-22 13:29:27 +1300

    meow meow

--- Chat_client.py
+++ Chat_client.py
@@ -1,13 +1,22 @@
-﻿import ssl
-
-import select
+﻿import select
 import socket
 import sys
 import signal
 import argparse
+import threading
+
 from utils import *
 
 SERVER_HOST = 'localhost'
+
+stop_thread = False
+
+
+def get_and_send(client):
+    while not stop_thread:
+        data = sys.stdin.readline().strip()
+        if data:
+            send(client.sock, data)
 
 
 class ChatClient():
@@ -18,7 +27,6 @@ class ChatClient():
         self.connected = False
         self.host = host
         self.port = port
-        self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 
         # Initial prompt
         self.prompt = f'[{name}@{socket.gethostname()}]> '
@@ -26,8 +34,6 @@ class ChatClient():
         # Connect to server at port
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock = self.context.wrap_socket(
-                self.sock, server_hostname=host)
             self.sock.connect((host, self.port))
             print(f'Now connected to chat server@ port {self.port}')
             self.connected = True
@@ -39,6 +45,9 @@ class ChatClient():
             # Contains client address, set it
             addr = data.split('CLIENT: ')[1]
             self.prompt = '[' + '@'.join((self.name, addr)) + ']> '
+
+            threading.Thread(target=get_and_send, args=(self,)).start()
+
         except socket.error as e:
             print(f'Failed to connect to chat server @ port {self.port}')
             sys.exit(1)
@@ -55,13 +64,16 @@ class ChatClient():
                 sys.stdout.flush()
 
                 # Wait for input from stdin and socket
-                readable, writeable, exceptional = select.select([0, self.sock], [], [])
+                # readable, writeable, exceptional = select.select([0, self.sock], [], [])
+                readable, writeable, exceptional = select.select(
+                    [self.sock], [], [])
+
                 for sock in readable:
-                    if sock == 0:
-                        data = sys.stdin.readline().strip()
-                        if data:
-                            send(self.sock, data)
-                    elif sock == self.sock:
+                    # if sock == 0:
+                    #     data = sys.stdin.readline().strip()
+                    #     if data:
+                    #         send(self.sock, data)
+                    if sock == self.sock:
                         data = receive(self.sock)
                         if not data:
                             print('Client shutting down.')
@@ -72,7 +84,8 @@ class ChatClient():
                             sys.stdout.flush()
 
             except KeyboardInterrupt:
-                print(" Client interrupted. ")
+                print(" Client interrupted. " "")
+                stop_thread = True
                 self.cleanup()
                 break
 
@@ -80,9 +93,10 @@ class ChatClient():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', action="store", dest="name", required=True)
-    parser.add_argument('--port', action="store", dest="port", type=int, required=True)
-
+    parser.add_argument('--port', action="store",
+                        dest="port", type=int, required=True)
     given_args = parser.parse_args()
+
     port = given_args.port
     name = given_args.name
 
--- Chat_server.py
+++ Chat_server.py
@@ -1,10 +1,9 @@
-﻿import ssl
-
-import select
+﻿import select
 import socket
 import sys
 import signal
 import argparse
+
 from utils import *
 
 SERVER_HOST = 'localhost'
@@ -17,25 +16,23 @@ class ChatServer(object):
         self.clients = 0
         self.clientmap = {}
         self.outputs = []  # list output sockets
-        self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-        self.context.load_cert_chain(certfile="cert.pem", keyfile="cert.pem")
-        self.context.load_verify_locations('cert.pem')
-        self.context.set_ciphers('AES128-SHA')
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((SERVER_HOST, port))
         self.server.listen(backlog)
-        self.server = self.context.wrap_socket(self.server, server_side=True)
         # Catch keyboard interrupts
         signal.signal(signal.SIGINT, self.sighandler)
+
         print(f'Server listening to port: {port} ...')
 
     def sighandler(self, signum, frame):
-        """ Clean up client outputs """
+        """ Clean up client outputs"""
         print('Shutting down server...')
+
         # Close existing client sockets
         for output in self.outputs:
             output.close()
+
         self.server.close()
 
     def get_client_name(self, client):
@@ -45,24 +42,24 @@ class ChatServer(object):
         return '@'.join((name, host))
 
     def run(self):
-        inputs = [self.server, sys.stdin]
+        # inputs = [self.server, sys.stdin]
+        inputs = [self.server]
         self.outputs = []
         running = True
-
         while running:
             try:
-                readable, writeable, exceptional = select.select(inputs, self.outputs, [])
+                readable, writeable, exceptional = select.select(
+                    inputs, self.outputs, [])
             except select.error as e:
                 break
 
             for sock in readable:
                 sys.stdout.flush()
-
                 if sock == self.server:
                     # handle the server socket
                     client, address = self.server.accept()
-                    print(f'Chat server: got connection {client.fileno()} from {address}')
-
+                    print(
+                        f'Chat server: got connection {client.fileno()} from {address}')
                     # Read the login name
                     cname = receive(client).split('NAME: ')[1]
 
@@ -70,23 +67,22 @@ class ChatServer(object):
                     self.clients += 1
                     send(client, f'CLIENT: {str(address[0])}')
                     inputs.append(client)
-                    self.clientmap[client] = (address, cname)
 
+                    self.clientmap[client] = (address, cname)
                     # Send joining information to other clients
                     msg = f'\n(Connected: New client ({self.clients}) from {self.get_client_name(client)})'
                     for output in self.outputs:
                         send(output, msg)
-
                     self.outputs.append(client)
 
-                elif sock == sys.stdin:
-                    # handle standard input
-                    cmd = sys.stdin.readline().strip()
-                    if cmd == 'list':
-                        print(self.clientmap.values())
-                    elif cmd == 'quit':
-                        running = False
-
+                # elif sock == sys.stdin:
+                #     # didn't test sys.stdin on windows system
+                #     # handle standard input
+                #     cmd = sys.stdin.readline().strip()
+                #     if cmd == 'list':
+                #         print(self.clientmap.values())
+                #     elif cmd == 'quit':
+                #         running = False
                 else:
                     # handle all other sockets
                     try:
@@ -94,6 +90,7 @@ class ChatServer(object):
                         if data:
                             # Send as new client's message...
                             msg = f'\n#[{self.get_client_name(sock)}]>> {data}'
+
                             # Send data to all except ourself
                             for output in self.outputs:
                                 if output != sock:
@@ -107,22 +104,26 @@ class ChatServer(object):
 
                             # Sending client leaving information to others
                             msg = f'\n(Now hung up: Client from {self.get_client_name(sock)})'
+
                             for output in self.outputs:
                                 send(output, msg)
-
                     except socket.error as e:
                         # Remove
                         inputs.remove(sock)
                         self.outputs.remove(sock)
-                        self.server.close()
+
+        self.server.close()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Socket Server Example with Select')
+    parser = argparse.ArgumentParser(
+        description='Socket Server Example with Select')
     parser.add_argument('--name', action="store", dest="name", required=True)
-    parser.add_argument('--port', action="store", dest="port", type=int, required=True)
+    parser.add_argument('--port', action="store",
+                        dest="port", type=int, required=True)
     given_args = parser.parse_args()
     port = given_args.port
     name = given_args.name
+
     server = ChatServer(port)
     server.run()
